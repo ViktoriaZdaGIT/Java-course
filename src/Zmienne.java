113f217778c70df6336111693b9324fd523b2561
Viktoria <wiktoria@gmail.com>
2024-03-18 06:29:00 +0100

    Zmienne

---
+++ src/Zmienne.java
@@ -0,0 +1,14 @@
+// zarezrwowane miejsce w pamięcie z określonym typem danych
+public class Zmienne {
+
+    public static void main(String[] args) {
+      String name = "Viktoria";
+      int firstNumber = 1;
+      int secondNumber = 2;
+      int result = firstNumber + secondNumber;
+
+      name = "Robert";
+      System.out.println(name);
+        System.out.println(result);
+    }
+}
