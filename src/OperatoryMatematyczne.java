ca403ca7c1dec7af5cc0dac09386b5cce4a62549
Viktoria <wiktoria@gmail.com>
2024-03-18 06:28:24 +0100

    Operatory Matematyczne

---
+++ src/OperatoryMatematyczne.java
@@ -0,0 +1,22 @@
+// umożliwiają wykonanie podstawowych operacji matematycznych
+public class OperatoryMatematyczne {
+
+    public static void main(String[] args) {
+        int firstNumber = 4;
+        int secondNumber = 6;
+        float thirdNumber = 4.0F;
+
+        int addition = firstNumber + secondNumber; //10
+        int subtraction = firstNumber - secondNumber; // -2
+        int multiplication = firstNumber * secondNumber; // 24
+        float division = thirdNumber/secondNumber; // 0,66
+        int mod = secondNumber%firstNumber; // ile razy 4 mieści się w 6? odp 1 i reszta z dzielenia jest 2
+
+        System.out.println("Wynik dodawania " + addition); //+
+        System.out.println("Odejmowanie " + subtraction); //+
+        System.out.println("Mnożenie " + multiplication); //+
+        System.out.println("Dzielenie " + division); //+
+        System.out.println("Modulo " + mod); //+
+
+    }
+}
