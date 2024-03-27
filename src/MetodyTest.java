1d92d5c107004c57b69a87599bfa1cfee1c3c439
Viktoria <wiktoria@gmail.com>
2024-03-27 10:38:25 +0100

    Sample commit mesage

---
+++ src/MetodyTest.java
@@ -0,0 +1,25 @@
+public class MetodyTest {
+
+    public static void main(String[] args) {
+        Metody metody = new Metody();
+/*      metody.policzWynik();
+        metody.policzWynik();
+        metody.policzWynik();
+        metody.policzWynik();
+        int result = metody.pobierzWynik();
+        int result2 = result *2;
+        System.out.println("Rezulta przed mnożeniem to: " + result);
+        System.out.println("Rezulta po mnożeniu to: " + result2);*/
+
+/*        metody.policzWynikParam(2);
+        metody.policzWynikParam( 100);
+        metody.policzWynikParam(  23);*/
+
+
+        int result = metody.add (2,3);
+        System.out.println(metody.add(2,6));
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/StudentChecker_ZD.java
@@ -0,0 +1,43 @@
+// Stworz 3 obiekty klasy student. Przypisz wartosci do pol. Stworz tablice i dodaj studentow do tablicy.
+// Przejdz przez studentow w tablicy i wywolaj wszystkie 4 metody
+
+public class StudentChecker_ZD {
+    public static void main(String[] args) {
+
+       Student student1 = new Student ();
+       student1.imie = "Katarzyna";
+       student1.nazwisko ="Komarowska";
+       student1.nick = "Kasia";
+       student1.email = "kasiakomar@gmail.com";
+       student1.numerIndeksu = 123456;
+
+       Student student2  = new Student ();
+       student2.imie = "Bartosz";
+       student2.nazwisko ="Komar";
+       student2.nick = "Bartek";
+       student2.email = "bartekkomar@gmail.com";
+       student2.numerIndeksu = 654321;
+
+       Student student3  = new Student ();
+       student3.imie = "Alicja";
+       student3.nazwisko ="Koper";
+       student3.nick = "Ala";
+       student3.email = "alakoper@gmail.com";
+       student3.numerIndeksu = 78906;
+
+
+       Student [] students = new Student[3];
+       students [0] = student1;
+       students [1] = student2;
+       students [2] = student3;
+
+       for (int i=0; i<students.length; i++) {
+          students[i].przedstawSie();
+          students[i].podajEmail();
+          students[i].podajNrIndeksu();
+          students[i].zalogujSie();
+       }
+
+    }
+
+}
